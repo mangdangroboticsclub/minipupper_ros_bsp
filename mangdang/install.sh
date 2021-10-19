f67340d9a0bae226aa948a6f81e7df21bd748cdb
Jack Wang <fae@mangdang.net>
2021-10-20 07:40:33 +0800

    SCM: The first upload for MiniPupper ROS

---
+++ mangdang/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# Install Mangdang driever
+
+# build and deploy battery monitor deamon and IO configuration
+sudo cp PWMController/i2c-pwm-pca9685a.dtbo /boot/firmware/overlays/
+sudo cp IO_Configuration/syscfg.txt /boot/firmware/ -f
+sudo cp LCD/cartoons/* /home/ubuntu/Pictures/
+
+cd /home/ubuntu/MiniPupperROS/mangdang/BatteryMonitor
+cmake .
+make
+sudo bash install.sh
+
+# install lcd service
+sudo bash /home/ubuntu/MiniPupperROS/mangdang/LCD/install.sh
+
+
