9be0de4d24793c6679b8fd9a5e756c08d22dd21b
James.Wang@mangdang.net <fae@mangdang.net>
2022-02-26 13:45:31 +0800

    BSP: FG: Open the source code of Fuel gauge

--- mangdang/FuelGauge/install.sh
+++ mangdang/FuelGauge/install.sh
@@ -4,8 +4,9 @@
 
 set -x
 sudo cp i2c4.dtbo /boot/firmware/overlays/
-sudo cp max1720x_battery.ko /lib/modules/`uname -r`/kernel/drivers/power/supply/
-sudo depmod -a
+
+sudo make
+sudo make install
 
 sudo cp battery_monitor /usr/bin/
 sudo cp battery_monitor.service /lib/systemd/system/
